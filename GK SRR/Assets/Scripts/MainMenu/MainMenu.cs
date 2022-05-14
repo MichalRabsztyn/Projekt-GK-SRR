8583795f2a50cfc9c67bc2f7e830c4cfed90559c
MichalRabsztyn <56251933+MichalRabsztyn@users.noreply.github.com>
2022-05-14 19:12:50 +0200

    Main Menu

---
+++ GK SRR/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,24 @@
+/**
+ * Main menu controls
+ * 
+ * Author: mrabsztyn
+ **/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
